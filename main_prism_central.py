00be7ed9a2fb90587ebea7f80df695e2c7847429
izecevic <igor.zecevic@nutanix.com>
2023-01-09 14:18:52 +0100

    updates

--- main_prism_central.py
+++ main_prism_central.py
@@ -27,182 +27,181 @@ pc_directory = json_config['directory'][0]['name']
 
 # main
 
-# # region update prism admin default password
-# print("--- Update prism default password section ---")
-# pc_cluster_details = prism_get_cluster(pc_api,pc_user,pc_pwd)
-# if pc_cluster_details == 401: # (UNAUTHORIZED)
-#     prism_update_default_pwd(pc_api,new_secret=pc_pwd,username="admin",default_secret="Nutanix/4u")
-# else:
-#     print("Default password already updated on PC {}".format(pc_api))
-# # end region
+# region update prism admin default password
+print("--- Update prism default password section ---")
+pc_cluster_details = prism_get_cluster(pc_api,pc_user,pc_pwd)
+if pc_cluster_details == 401: # (UNAUTHORIZED)
+    prism_update_default_pwd(pc_api,new_secret=pc_pwd,username="admin",default_secret="Nutanix/4u")
+else:
+    print("Default password already updated on PC {}".format(pc_api))
+# endregion
 
-# # region eulas
-# print("\n--- Eulas section ---")
-# pc_eula_details = prism_get_eula(pc_api,pc_user,pc_pwd)
-# if 'userDetailsList' not in pc_eula_details['entities'][0]:
-#     prism_accept_eula(pc_api,pc_user,pc_pwd,pc_eula['username'],pc_eula['company'],pc_eula['jobtitle'])
-# else:
-#     print("Eulas aready accepted on PC {}".format(pc_api))
-# # endregion
+# region eulas
+print("\n--- Eulas section ---")
+pc_eula_details = prism_get_eula(pc_api,pc_user,pc_pwd)
+if 'userDetailsList' not in pc_eula_details['entities'][0]:
+    prism_accept_eula(pc_api,pc_user,pc_pwd,pc_eula['username'],pc_eula['company'],pc_eula['jobtitle'])
+else:
+    print("Eulas aready accepted on PC {}".format(pc_api))
+# endregion
 
-# # region pulse
-# print("\n--- Pulse section ---")
-# pc_pulse_status = prism_get_pulse(pc_api,pc_user,pc_pwd)
-# if pc_pulse_status['isPulsePromptNeeded'] != False or pc_pulse_status['enable'] != pc_pulse:
-#     prism_enable_pulse(pc_api,pc_user,pc_pwd,pc_pulse)
-# else:
-#     print("Pulse already configured on PC {}".format(pc_api))
-# # endregion
+# region pulse
+print("\n--- Pulse section ---")
+pc_pulse_status = prism_get_pulse(pc_api,pc_user,pc_pwd)
+if pc_pulse_status['isPulsePromptNeeded'] != False or pc_pulse_status['enable'] != pc_pulse:
+    prism_enable_pulse(pc_api,pc_user,pc_pwd,pc_pulse)
+else:
+    print("Pulse already configured on PC {}".format(pc_api))
+# endregion
 
-# # region dns
-# print("\n--- DNS section ---")
-# pc_dns_details = prism_get_dns(pc_api,pc_user,pc_pwd)
-# for dns_server in cluster_dns:
-#     if dns_server not in pc_dns_details:
-#         prism_add_dns(pc_api,pc_user,pc_pwd,dns_server)
-#     else:
-#         print("DNS {} already configured on PC {}".format(dns_server,pc_api))
-# # endregion
+# region dns
+print("\n--- DNS section ---")
+pc_dns_details = prism_get_dns(pc_api,pc_user,pc_pwd)
+for dns_server in cluster_dns:
+    if dns_server not in pc_dns_details:
+        prism_add_dns(pc_api,pc_user,pc_pwd,dns_server)
+    else:
+        print("DNS {} already configured on PC {}".format(dns_server,pc_api))
+# endregion
 
-# # region ntp
-# print("\n--- NTP section ---")
-# pc_ntp_details = prism_get_ntp(pc_api,pc_user,pc_pwd)
-# for ntp_server in cluster_ntp:
-#     if ntp_server not in pc_ntp_details:
-#         prism_add_ntp(pc_api,pc_user,pc_pwd,ntp_server)
-#     else:
-#         print("NTP {} already configured on PC {}".format(ntp_server,pc_api))
-# # endregion
+# region ntp
+print("\n--- NTP section ---")
+pc_ntp_details = prism_get_ntp(pc_api,pc_user,pc_pwd)
+for ntp_server in cluster_ntp:
+    if ntp_server not in pc_ntp_details:
+        prism_add_ntp(pc_api,pc_user,pc_pwd,ntp_server)
+    else:
+        print("NTP {} already configured on PC {}".format(ntp_server,pc_api))
+# endregion
 
-# #region AD
-# print("\n--- AD section ---")
-# for directory in cluster_directories:
-#     pc_directory_details = prism_get_directory(pc_api,pc_user,pc_pwd,directory['domain'])
-#     if pc_directory_details == None:
-#         prism_set_directory(pc_api,pc_user,pc_pwd,directory['name'],directory['domain'],directory['url'],directory['svc_user'],directory['svc_pwd'])
-#     else:
-#         print("Directory {} already configured on PC {}".format(directory['domain'],pc_api))
-# # endregion
+#region AD
+print("\n--- AD section ---")
+for directory in cluster_directories:
+    pc_directory_details = prism_get_directory(pc_api,pc_user,pc_pwd,directory['domain'])
+    if pc_directory_details == None:
+        prism_set_directory(pc_api,pc_user,pc_pwd,directory['name'],directory['domain'],directory['url'],directory['svc_user'],directory['svc_pwd'])
+    else:
+        print("Directory {} already configured on PC {}".format(directory['domain'],pc_api))
+# endregion
 
-# # region role mapping
-# print("\n--- Role mapping section ---")
-# for directory in cluster_directories:
-#     pc_directory_details = prism_get_directory(pc_api,pc_user,pc_pwd,directory['domain'])
-#     if pc_directory_details:
-#         pc_role_mappings_details = prism_get_role_mappings(pc_api,pc_user,pc_pwd,directory['name'])
-#         if not pc_role_mappings_details:
-#             for role in directory['role_mapping']:
-#                 prism_set_role_mapping(pc_api,pc_user,pc_pwd,directory['name'],role['name'],role['type'],role['value'])
-#         else:
-#             for role in directory['role_mapping']:
-#                 role_exist = False
-#                 for role_detail in pc_role_mappings_details:
-#                     if role['name'] in role_detail['role']:
-#                         role_exist = True # role exist
-#                 if role_exist == False:
-#                     prism_set_role_mapping(pc_api,pc_user,pc_pwd,directory['name'],role['name'],role['type'],role['value'])
-#                 else:
-#                     print("Role {} already configured on PC {}".format(role['name'],pc_api))
-#     else:
-#         print("Directory {} not configured on PC {}".format(directory['domain'],pc_api))
-# # endregion
+# region role mapping
+print("\n--- Role mapping section ---")
+for directory in cluster_directories:
+    pc_directory_details = prism_get_directory(pc_api,pc_user,pc_pwd,directory['domain'])
+    if pc_directory_details:
+        pc_role_mappings_details = prism_get_role_mappings(pc_api,pc_user,pc_pwd,directory['name'])
+        if not pc_role_mappings_details:
+            for role in directory['role_mapping']:
+                prism_set_role_mapping(pc_api,pc_user,pc_pwd,directory['name'],role['name'],role['type'],role['value'])
+        else:
+            for role in directory['role_mapping']:
+                role_exist = False
+                for role_detail in pc_role_mappings_details:
+                    if role['name'] in role_detail['role']:
+                        role_exist = True # role exist
+                if role_exist == False:
+                    prism_set_role_mapping(pc_api,pc_user,pc_pwd,directory['name'],role['name'],role['type'],role['value'])
+                else:
+                    print("Role {} already configured on PC {}".format(role['name'],pc_api))
+    else:
+        print("Directory {} not configured on PC {}".format(directory['domain'],pc_api))
+# endregion
 
-# #region role
-# print("\n--- Role section ---")
-# permissions_uuid_list = []
-# for role in pc_roles:
-#     role_name = role['name']
-#     pc_role_details = pc_get_roles(pc_api,pc_user,pc_pwd,role_name)
-#     if not pc_role_details:
-#         print ("Creating role {} on PC {}".format(role_name,pc_api))
-#         for permission in role['permissions']:
-#             permission_uuid = pc_get_permission_uuid(pc_api,pc_user,pc_pwd,permission)
-#             permissions_uuid_list.append(permission_uuid) # push each permission in a list
-#         pc_role_task = pc_create_role(pc_api,pc_user,pc_pwd,role_name,permissions_uuid_list)
-#         pc_role_task_uuid = pc_role_task['status']['execution_context']['task_uuid']
-#         prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_role_task_uuid,retry_delay_secs=10,max_attemps=10)
-#     else:
-#         print("Role {} already created on PC {}".format(role_name,pc_api))
-# #endregion
+#region role
+print("\n--- Role section ---")
+permissions_uuid_list = []
+for role in pc_roles:
+    role_name = role['name']
+    pc_role_details = pc_get_roles(pc_api,pc_user,pc_pwd,role_name)
+    if not pc_role_details:
+        print ("Creating role {} on PC {}".format(role_name,pc_api))
+        for permission in role['permissions']:
+            permission_uuid = pc_get_permission_uuid(pc_api,pc_user,pc_pwd,permission)
+            permissions_uuid_list.append(permission_uuid) # push each permission in a list
+        pc_role_task = pc_create_role(pc_api,pc_user,pc_pwd,role_name,permissions_uuid_list)
+        pc_role_task_uuid = pc_role_task['status']['execution_context']['task_uuid']
+        prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_role_task_uuid,retry_delay_secs=10,max_attemps=10)
+    else:
+        print("Role {} already created on PC {}".format(role_name,pc_api))
+#endregion
 
 
 # region import images from PE to PC
-# print("\n--- Import images section ---")
-# prism_cluster_uuid = pc_get_cluster_uuid(pc_api,pc_user,pc_pwd,cluster_name)
-# pc_prism_get_images = prism_get_images(prism_api,prism_user,prism_pwd)
-# for image in pc_prism_get_images:
-#     image_name = image['name']
-#     image_uuid = image['uuid']
-#     pc_image_details = pc_get_image_by_uuid(pc_api,pc_user,pc_pwd,image_uuid)
-#     if pc_image_details == 404: # entity not found
-#         pc_import_image_task = pc_import_image_from_pe(pc_api,pc_user,pc_pwd,prism_cluster_uuid)
-#         pc_import_image_task_uuid = json.loads(pc_import_image_task['api_response_list'][0]['api_response'])['task_uuid']
-#         prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_import_image_task_uuid,retry_delay_secs=10,max_attemps=10)
-#     elif pc_image_details and pc_image_details != 404:
-#         print("Image {} already imported on PC {}".format(image_name,pc_api))
+print("\n--- Import images section ---")
+prism_cluster_uuid = pc_get_cluster_uuid(pc_api,pc_user,pc_pwd,cluster_name)
+pc_prism_get_images = prism_get_images(prism_api,prism_user,prism_pwd)
+for image in pc_prism_get_images:
+    image_name = image['name']
+    image_uuid = image['uuid']
+    pc_image_details = pc_get_image_by_uuid(pc_api,pc_user,pc_pwd,image_uuid)
+    if pc_image_details == 404: # entity not found
+        pc_import_image_task = pc_import_image_from_pe(pc_api,pc_user,pc_pwd,prism_cluster_uuid)
+        pc_import_image_task_uuid = json.loads(pc_import_image_task['api_response_list'][0]['api_response'])['task_uuid']
+        prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_import_image_task_uuid,retry_delay_secs=10,max_attemps=10)
+    elif pc_image_details and pc_image_details != 404:
+        print("Image {} already imported on PC {}".format(image_name,pc_api))
 # endregion
  
 # region enable calm
-# print("\n--- Enable Calm section ---")
-# pc_calm_status = pc_check_calm(pc_api,pc_user,pc_pwd)
-# print(json.dumps(pc_calm_status,indent=4))
-# if pc_calm_status['service_enablement_status'] != "ENABLED":
-#     pc_enable_calm_task = pc_enable_calm(pc_api,pc_user ,pc_pwd)
-#     pc_enable_calm_task_uuid = pc_enable_calm_task['task_uuid']
-#     prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_enable_calm_task_uuid,retry_delay_secs=30,max_attemps=10)
-# else:
-#     print("Calm already enabled on PC {}".format(pc_api))
+print("\n--- Enable Calm section ---")
+pc_calm_status = pc_check_calm(pc_api,pc_user,pc_pwd)
+print(json.dumps(pc_calm_status,indent=4))
+if pc_calm_status['service_enablement_status'] != "ENABLED":
+    pc_enable_calm_task = pc_enable_calm(pc_api,pc_user ,pc_pwd)
+    pc_enable_calm_task_uuid = pc_enable_calm_task['task_uuid']
+    prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_enable_calm_task_uuid,retry_delay_secs=30,max_attemps=10)
+else:
+    print("Calm already enabled on PC {}".format(pc_api))
 # endregion
 
-# # region create calm project
-# print("\n--- Create Calm project section ---")
-# for project in pc_calm_projects:
-#     project_name = project['project_name']
-#     pc_projects_details = pc_get_projects(pc_api,pc_user,pc_pwd,project_name)
-#     if not pc_projects_details:
-#         pc_create_project_task = pc_create_project(pc_api,pc_user,pc_pwd,project_name)
-#         pc_create_project_task_uuid = pc_create_project_task['status']['execution_context']['task_uuid']
-#         prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_create_project_task_uuid,retry_delay_secs=10,max_attemps=6)
-#     else:
-#         print("Project {} already created on PC {}".format(project_name,pc_api))
-# # endregion
+# region create calm project
+print("\n--- Create Calm project section ---")
+for project in pc_calm_projects:
+    project_name = project['project_name']
+    pc_projects_details = pc_get_projects(pc_api,pc_user,pc_pwd,project_name)
+    if not pc_projects_details:
+        pc_create_project_task = pc_create_project(pc_api,pc_user,pc_pwd,project_name)
+        pc_create_project_task_uuid = pc_create_project_task['status']['execution_context']['task_uuid']
+        prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_create_project_task_uuid,retry_delay_secs=10,max_attemps=6)
+    else:
+        print("Project {} already created on PC {}".format(project_name,pc_api))
+# endregion
 
-# # region update permissions calm project
-# print("\n--- Update permission Calm project section ---")
-# for project in pc_calm_projects:
-#     project_name = project['project_name']
-#     pc_project_uuid = pc_get_project_uuid(pc_api,pc_user,pc_pwd,project_name)
-#     if pc_project_uuid:
-#         pc_project_internal_details = pc_get_projects_internal(pc_api,pc_user,pc_pwd,pc_project_uuid)
-#         if not pc_project_internal_details['spec']['access_control_policy_list']:
-#             for permission in project['project_permissions']:
-#                 directory_service_name=permission['directory']
-#                 pc_directory_uuid = pc_get_directory_service_uuid(pc_api,pc_user,pc_pwd,directory_service_name)
-#                 if permission['type'] == "group":
-#                     group_name = permission['name']
-#                     role_name = permission['role']
-#                     pc_dn_group = pc_calm_search_users(pc_api,pc_user,pc_pwd,pc_directory_uuid,group_name)
-#                     pc_acp_group_id = pc_get_acp_group_id(pc_api,pc_user,pc_pwd,pc_dn_group)
-#                     pc_group_role_uuid = pc_get_role_uuid(pc_api,pc_user,pc_pwd,role_name)
-#                     pc_set_project_acp_group_task = pc_set_project_acp_group(pc_api,pc_user,pc_pwd,pc_project_uuid,pc_acp_group_id,pc_group_role_uuid)
-#                     pc_set_project_acp_group_task_uuid = pc_set_project_acp_group_task['status']['execution_context']['task_uuid']
-#                     prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_set_project_acp_group_task_uuid,retry_delay_secs=10,max_attemps=6)
-#                 if permission['type'] == "user":
-#                     user_name = permission['name']
-#                     role_name = permission['role']
-#                     pc_dn_user = pc_calm_search_users(pc_api,pc_user,pc_pwd,pc_directory_uuid,user_name)
-#                     pc_acp_user_id = pc_get_acp_user_id(pc_api,pc_user,pc_pwd,pc_dn_user)
-#                     pc_user_role_uuid = pc_get_role_uuid(pc_api,pc_user,pc_pwd,role_name)
-#                     pc_set_project_acp_user_task = pc_set_project_acp_user(pc_api,pc_user,pc_pwd,pc_project_uuid,pc_acp_user_id,pc_user_role_uuid)
-#                     pc_set_project_acp_user_task_uuid = pc_set_project_acp_group_task['status']['execution_context']['task_uuid']
-#                     prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_set_project_acp_user_task_uuid,retry_delay_secs=10,max_attemps=6)
+# region update permissions calm project
+print("\n--- Update permission Calm project section ---")
+for project in pc_calm_projects:
+    project_name = project['project_name']
+    pc_project_uuid = pc_get_project_uuid(pc_api,pc_user,pc_pwd,project_name)
+    if pc_project_uuid:
+        pc_project_internal_details = pc_get_projects_internal(pc_api,pc_user,pc_pwd,pc_project_uuid)
+        if not pc_project_internal_details['spec']['access_control_policy_list']:
+            for permission in project['project_permissions']:
+                directory_service_name=permission['directory']
+                pc_directory_uuid = pc_get_directory_service_uuid(pc_api,pc_user,pc_pwd,directory_service_name)
+                if permission['type'] == "group":
+                    group_name = permission['name']
+                    role_name = permission['role']
+                    pc_dn_group = pc_calm_search_users(pc_api,pc_user,pc_pwd,pc_directory_uuid,group_name)
+                    pc_acp_group_id = pc_get_acp_group_id(pc_api,pc_user,pc_pwd,pc_dn_group)
+                    pc_group_role_uuid = pc_get_role_uuid(pc_api,pc_user,pc_pwd,role_name)
+                    pc_set_project_acp_group_task = pc_set_project_acp_group(pc_api,pc_user,pc_pwd,pc_project_uuid,pc_acp_group_id,pc_group_role_uuid)
+                    pc_set_project_acp_group_task_uuid = pc_set_project_acp_group_task['status']['execution_context']['task_uuid']
+                    prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_set_project_acp_group_task_uuid,retry_delay_secs=10,max_attemps=6)
+                if permission['type'] == "user":
+                    user_name = permission['name']
+                    role_name = permission['role']
+                    pc_dn_user = pc_calm_search_users(pc_api,pc_user,pc_pwd,pc_directory_uuid,user_name)
+                    pc_acp_user_id = pc_get_acp_user_id(pc_api,pc_user,pc_pwd,pc_dn_user)
+                    pc_user_role_uuid = pc_get_role_uuid(pc_api,pc_user,pc_pwd,role_name)
+                    pc_set_project_acp_user_task = pc_set_project_acp_user(pc_api,pc_user,pc_pwd,pc_project_uuid,pc_acp_user_id,pc_user_role_uuid)
+                    pc_set_project_acp_user_task_uuid = pc_set_project_acp_group_task['status']['execution_context']['task_uuid']
+                    prism_monitor_task_v2(pc_api,pc_user,pc_pwd,pc_set_project_acp_user_task_uuid,retry_delay_secs=10,max_attemps=6)
 
-#         else:
-#             print("Permissions already set for project {}".format(project_name))
-#     else:
-#         print("Project {} doesn't exist on PC {} - Skipping".format(project_name,pc_api))
-
-# # endregion
+        else:
+            print("Permissions already set for project {}".format(project_name))
+    else:
+        print("Project {} doesn't exist on PC {} - Skipping".format(project_name,pc_api))
+# endregion
 
 # region update infrastructure calm project
 print("\n--- Update infrastructure Calm project section ---")
@@ -260,7 +259,6 @@ for project in pc_calm_projects:
         print("Runbook {} already imported on Caln {}".format(runbook_name,pc_api))
 
 # endregion
-
 
 # region marketplace calm
 
